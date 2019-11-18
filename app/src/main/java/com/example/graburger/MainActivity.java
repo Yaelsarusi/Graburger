2c7d19849007066b5a619afb1de2e3a39d961a95
shakedweitz <shaked.weitz@mail.huji.ac.il>
2019-11-18 10:43:40 +0200

    Main activity: * Add Carousel Picker * Checkout button
    drawable:
    * Round button style

--- app/src/main/java/com/example/graburger/MainActivity.java
+++ app/src/main/java/com/example/graburger/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.graburger;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.goodiebag.carouselpicker.CarouselPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<FoodModel> foods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarouselPicker carouselPicker = (CarouselPicker) findViewById(R.id.carousel);
+
+        List<CarouselPicker.PickerItem> imageItems = new ArrayList<>();
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.fries));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.burger));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.soda));
+        //Create an adapter
+        CarouselPicker.CarouselViewAdapter imageAdapter = new CarouselPicker.CarouselViewAdapter(this, imageItems, 0);
+        //Set the adapter
+        carouselPicker.setAdapter(imageAdapter);
+
+        carouselPicker.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //position of the selected item
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+    public void launchCheckoutDialog(View view) {
     }
 }
