af0b313f5702d4452a0ee060d1e3b97c48a58fcb
yael-sarusi <yael.sarusi@mail.huji.ac.il>
2019-11-23 17:15:38 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/5.1.1/executionHistory/executionHistory.bin
    #	.gradle/5.1.1/executionHistory/executionHistory.lock
    #	.gradle/5.1.1/fileHashes/fileHashes.bin
    #	.gradle/5.1.1/fileHashes/fileHashes.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	.idea/caches/build_file_checksums.ser
    #	.idea/caches/gradle_models.ser
    #	.idea/libraries/Gradle__android_arch_core_runtime_1_1_1_aar.xml
    #	.idea/libraries/Gradle__android_arch_lifecycle_livedata_1_1_1_aar.xml
    #	.idea/libraries/Gradle__android_arch_lifecycle_livedata_core_1_1_1_aar.xml
    #	.idea/libraries/Gradle__android_arch_lifecycle_runtime_1_1_1_aar.xml
    #	.idea/libraries/Gradle__android_arch_lifecycle_viewmodel_1_1_1_aar.xml
    #	.idea/libraries/Gradle__com_android_support_animated_vector_drawable_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_appcompat_v7_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_asynclayoutinflater_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_constraint_constraint_layout_1_1_3_aar.xml
    #	.idea/libraries/Gradle__com_android_support_coordinatorlayout_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_cursoradapter_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_customview_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_documentfile_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_drawerlayout_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_interpolator_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_loader_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_localbroadcastmanager_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_print_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_slidingpanelayout_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_support_compat_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_support_core_ui_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_support_core_utils_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_support_fragment_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_support_vector_drawable_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_swiperefreshlayout_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_test_espresso_espresso_core_3_0_2_aar.xml
    #	.idea/libraries/Gradle__com_android_support_test_espresso_espresso_idling_resource_3_0_2_aar.xml
    #	.idea/libraries/Gradle__com_android_support_test_monitor_1_0_2_aar.xml
    #	.idea/libraries/Gradle__com_android_support_test_runner_1_0_2_aar.xml
    #	.idea/libraries/Gradle__com_android_support_versionedparcelable_28_0_0_aar.xml
    #	.idea/libraries/Gradle__com_android_support_viewpager_28_0_0_aar.xml
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml
    #	local.properties

--- app/src/main/java/com/example/graburger/buildBurgerActivity.java
+++ app/src/main/java/com/example/graburger/buildBurgerActivity.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 
