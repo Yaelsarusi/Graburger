1737967d4a6b7c16ae63e91a18da121e446efcba
yael-sarusi <yael.sarusi@mail.huji.ac.il>
2019-11-26 18:44:35 +0200

    fixed shit

--- app/src/main/java/com/example/graburger/MainActivity.java
+++ app/src/main/java/com/example/graburger/MainActivity.java
@@ -14,18 +14,22 @@ import in.goodiebag.carouselpicker.CarouselPicker;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewPager viewPager;
-    Adapter orderListAdapter;
+    int orderCarouselPosition;
+    ViewPager orderCarouselViewPager;
+    Adapter orderCarouselAdapter;
     BurgerItemModel curBurger;
     ArrayList<FoodItemModel> orderList;
-    int orderPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        orderList = new ArrayList<>();
 
+        createNewItemCarousel();
+        createOrderCarousel();
+    }
+
+    private void createNewItemCarousel() {
         CarouselPicker carouselPicker = (CarouselPicker) findViewById(R.id.carousel);
 
         List<CarouselPicker.PickerItem> imageItems = new ArrayList<>();
@@ -66,13 +70,84 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-        createOrderCarousel();
-
     }
 
+    private void createOrderCarousel() {
+        this.orderList = new ArrayList<>();
+        orderCarouselAdapter = new Adapter(orderList, this);
+        orderCarouselViewPager = findViewById(R.id.orderListPager);
+        orderCarouselViewPager.setAdapter(orderCarouselAdapter);
+        orderCarouselViewPager.setPadding(130, 0, 130, 0);
+        orderCarouselPosition = 0;
+        manageOrderCarouselView(false);
+
+        // TODO: need to add the burger description to the carousel
+
+        Button deleteButton = findViewById(R.id.deleteItemButton);
+        Button editButton = findViewById(R.id.editItemButton);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Edit button is shown only if the item is a burger, therefore we can assume at this point the item in the orderCarouselPosition is a burger.
+                BurgerItemModel curBurger = (BurgerItemModel)MainActivity.this.orderList.get(MainActivity.this.orderCarouselPosition);
+                Intent intent = new Intent(MainActivity.this, buildBurgerActivity.class);
+                intent.putExtra("curBurger", curBurger);
+                startActivityForResult(intent, buildBurgerActivity.EDIT_ACTIVITY_CODE);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MainActivity.this.orderList.remove(MainActivity.this.orderCarouselPosition);
+                manageOrderCarouselView(true);
+                orderCarouselViewPager.setCurrentItem(0);
+                MainActivity.this.orderCarouselPosition = 0;
+            }
+        });
+
+        orderCarouselViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //position of the selected item
+                MainActivity.this.orderCarouselPosition = position;
+                manageOrderCarouselView(false);}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // TODO : check if we need to implement
+            }
+
+        });
+    }
 
     public void launchCheckoutDialog(View view) {
+    }
+
+    private void manageOrderCarouselView(boolean dataChanged) {
+
+        // TODO: Carousel doesn't update the images after update.
+
+        Button deleteButton = findViewById(R.id.deleteItemButton);
+        Button editButton = findViewById(R.id.editItemButton);
+        if (MainActivity.this.orderList.isEmpty()){
+            deleteButton.setVisibility(View.GONE);
+            editButton.setVisibility(View.GONE);
+        } else {
+            deleteButton.setVisibility(View.VISIBLE);
+            if (this.orderList.get(this.orderCarouselPosition) instanceof BurgerItemModel){
+                editButton.setVisibility(View.VISIBLE);
+            }
+        }
+
+        if (dataChanged){
+            MainActivity.this.orderCarouselAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
@@ -85,75 +160,11 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == buildBurgerActivity.CREATE_NEW_ACTIVITY_CODE) {
                 orderList.add(orderedBurger);
             } else if (requestCode == buildBurgerActivity.EDIT_ACTIVITY_CODE) {
-                MainActivity.this.orderList.set(MainActivity.this.orderPosition, orderedBurger);
+                MainActivity.this.orderList.set(MainActivity.this.orderCarouselPosition, orderedBurger);
             }
-            orderListAdapter.notifyDataSetChanged();
-            manageOrderCarouselButtons(false);
-        }
-    }
-
-    private void createOrderCarousel() {
-        orderListAdapter = new Adapter(orderList, this);
-        viewPager = findViewById(R.id.orderListPager);
-        viewPager.setAdapter(orderListAdapter);
-        viewPager.setPadding(130, 0, 130, 0);
-        orderPosition = 0;
-        manageOrderCarouselButtons(true);
-
-        Button deleteButton = findViewById(R.id.deleteItemButton);
-        Button editButton = findViewById(R.id.editItemButton);
-
-        editButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Edit button is shown only if the item is a burger, therfore we can assume at this point the item in the orderPosition is a burger.
-                BurgerItemModel curBurger = (BurgerItemModel)MainActivity.this.orderList.get(MainActivity.this.orderPosition);
-                Intent intent = new Intent(MainActivity.this, buildBurgerActivity.class);
-                intent.putExtra("curBurger", curBurger);
-                startActivityForResult(intent, buildBurgerActivity.EDIT_ACTIVITY_CODE);
-            }
-        });
-
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MainActivity.this.orderList.remove(MainActivity.this.orderPosition);
-                MainActivity.this.orderListAdapter.notifyDataSetChanged();
-            }
-        });
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if (!MainActivity.this.orderList.isEmpty()){
-                    manageOrderCarouselButtons(false);
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                //position of the selected item
-                MainActivity.this.orderPosition = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                // TODO : check if we need to implement
-            }
-
-        });
-    }
-
-    private void manageOrderCarouselButtons(boolean hide) {
-        Button deleteButton = findViewById(R.id.deleteItemButton);
-        Button editButton = findViewById(R.id.editItemButton);
-        if (hide){
-            deleteButton.setVisibility(View.GONE);
-            editButton.setVisibility(View.GONE);
-        }
-        else {
-            deleteButton.setVisibility(View.VISIBLE);
-            // Todo - Show edit button only if current item in the carousel is burger
-            editButton.setVisibility(View.VISIBLE);
+            manageOrderCarouselView(true);
         }
     }
 }
+
+
