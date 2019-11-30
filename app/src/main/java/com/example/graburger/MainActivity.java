21d5ce3c0c3b4d47e7e451fedd1d0a10bd316245
yael-sarusi <yael.sarusi@mail.huji.ac.il>
2019-11-30 22:22:27 +0200

    Fixed minor bug, when scrolling through the carousel, the edit button didn't disappear if the item wasn't a hambuerger. Chaged the else condition and it is fixed now.

--- app/src/main/java/com/example/graburger/MainActivity.java
+++ app/src/main/java/com/example/graburger/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
