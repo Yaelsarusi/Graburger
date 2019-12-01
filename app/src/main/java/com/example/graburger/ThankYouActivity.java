61f848de459f36a4146511519636dbedf3af562d
daphnaleventer <daphna.leventer@huji.mail.ac.il>
2019-12-01 12:48:12 +0200

    Connect the checkout button to the thank you conffeti activity

---
+++ app/src/main/java/com/example/graburger/ThankYouActivity.java
@@ -0,0 +1,31 @@
+package com.example.graburger;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
+
+public class ThankYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you);
+        KonfettiView viewConfetti = findViewById(R.id.viewConfetti);
+        viewConfetti.build()
+                .addColors(Color.rgb(102, 255, 102), Color.rgb(153, 255, 153), Color.rgb(0, 204, 204))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(5000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12, 5))
+                .setPosition(+50f, viewConfetti.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 5000L);
+    }
+}
